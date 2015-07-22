086f97c64f478b0fe80c413c43dd235ed7e9121a
rafi <rafiber20@gmail.com>
2015-07-21 21:38:11 -0400

    anthonys game is cool

--- client/reactionTime/reactionTimer.js
+++ client/reactionTime/reactionTimer.js
@@ -1,35 +1,48 @@
+var starttime;
+var stoptime;
+var waitingTime=0;
+var running=false;
 var level=0;
 var levels= [1000, 750, 600, 500, 450, 300, 275, 250, 200];
+Session.set("level",0);
+
+
+Template.reactionTimer.helpers({
+	level:function(){return Session.get("level")},
+	maxlevel:function(){return levels[Session.get("level")]}
+})
+
 Template.reactionTimer.events({
 	"click #startgame": function(event){
 
 		if(!running){
-		responsetime=1000+Math.random()*5000; setTimeout(changecolor,responsetime); console.log(responsetime);
-		starttime=(new Date()).getTime()
-		running=true;}
-		else{running=false;$("#startgame").html("start!");
-		var stoptime=(new Date()).getTime();
-		var reactiontime= stoptime - starttime - responsetime
-		console.log("rt="+reactiontime); 
-		$("#time").html("rt="+(reactiontime))
+			//document.getElementById("startgame").disabled=true;
+			$("#startgame").fadeOut();
+			waitingTime=1000+Math.random()*5000;
+			setTimeout(enableButton,waitingTime); 
+			starttime=(new Date()).getTime();
+		}
+		else{
+			running=false;
+			$("#startgame").html("start!");
+			stoptime=(new Date()).getTime();
+			var reactiontime= Math.round(stoptime - starttime - waitingTime);
+			$("#time").html("rt="+reactiontime+" ms");
+		}
+
+		// if it is lower than 111 refresh page
 
 		if(reactiontime <= levels[level]){
 			level++;
-			$("#timeReq").html(levels[level] + " is the max to go to the next level");
-		}
-		
-
-	}
+			Session.set("level",level);
+		}		
 	}
 });
 
 
-
-Template.reactionTimer.rendered = function(){
-	$("#timeReq").html(levels[level] + " is the max to go to the next level");
+function enableButton(){
+	//document.getElementById("startgame").disabled=false;
+	$("#startgame").fadeIn();
+	$("#startgame").html("push me!");
+	running = true;
 }
-
-var starttime=0;
-var responsetime=0;
-var running=false;
-function changecolor(){$("#startgame").html("push me!")}
